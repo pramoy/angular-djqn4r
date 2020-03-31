54942ef6e640dbb6cd117a864e0558d5f605dbe8
pramoy <pramoy@gmail.com>
2020-03-31 12:57:33 +0530

    1 file modified added hirachical gantt fro testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,7 @@ export class AppComponent  implements OnInit {
   ngOnInit() {
     setTimeout(() => {
       this.ganttChart();
+      // this.ganttChartHira()
 
     }, 500);
   }
@@ -145,6 +146,104 @@ export class AppComponent  implements OnInit {
       }]
     });
 
+
+  }
+
+  ganttChartHira(){
+    let series_= [{"name":"Offices","data":[{"name":"New offices","id":"new_offices","owner":"Peter"},{"name":"Prepare office building","id":"prepare_building","parent":"new_offices","start":1585440000000,"end":1586131200000,"completed":{"amount":0.2},"owner":"Linda"},{"name":"Inspect building","id":"inspect_building","dependency":"prepare_building","parent":"new_offices","start":1586131200000,"end":1586304000000,"owner":"Ivy"},{"name":"Passed inspection","id":"passed_inspection","dependency":"inspect_building","parent":"new_offices","start":1586433600000,"milestone":true,"owner":"Peter"},{"name":"Relocate","id":"relocate","dependency":"passed_inspection","parent":"new_offices","owner":"Josh"},{"name":"Relocate staff","id":"relocate_staff","parent":"relocate","start":1586476800000,"end":1586563200000,"owner":"Mark"},{"name":"Relocate test facility","dependency":"relocate_staff","parent":"relocate","start":1586563200000,"end":1586736000000,"owner":"Anne"},{"name":"Relocate cantina","dependency":"relocate_staff","parent":"relocate","start":1586563200000,"end":1586822400000}]},{"name":"Product","data":[{"name":"New product launch","id":"new_product","owner":"Peter"},{"name":"Development","id":"development","parent":"new_product","start":1585526400000,"end":1586563200000,"completed":{"amount":0.6,"fill":"#e80"},"owner":"Susan"},{"name":"Beta","id":"beta","dependency":"development","parent":"new_product","start":1586692800000,"milestone":true,"owner":"Peter"},{"name":"Final development","id":"finalize","dependency":"beta","parent":"new_product","start":1586736000000,"end":1587081600000},{"name":"Launch","dependency":"finalize","parent":"new_product","start":1587124800000,"milestone":true,"owner":"Peter"}]}];
+this.taskChart = new Chart({
+      // Create the chart
+      // var chart = Highcharts.ganttChart('gnttcontainer', {
+
+      chart: {
+        // type: 'heatmap',
+        type: 'gantt',
+        spacingLeft: 1,
+        
+      },
+        plotOptions: {
+        series: {
+          animation: false, // Do not animate dependency connectors
+          // dragDrop: {
+          //   draggableX: true,
+          //   draggableY: true,
+          //   dragMinY: 0,
+          //   dragMaxY: 2,
+          //   dragPrecisionX: day / 3 // Snap to eight hours
+          // },
+          dataLabels: {
+            enabled: true,
+            format: '{point.name}',
+            style: {
+              cursor: 'default',
+              // pointerEvents: 'none'
+            }
+          },
+          allowPointSelect: true,
+          // point: {
+          //   events: {
+          //     select: updateRemoveButtonStatus,
+          //     unselect: updateRemoveButtonStatus,
+          //     remove: updateRemoveButtonStatus
+          //   }
+          // }
+        }
+      },
+series:series_,
+    // tooltip: {
+    //     pointFormatter: function () {
+    //         var point = this,
+    //             format = '%e. %b',
+    //             options = point.options,
+    //             completed = options.completed,
+    //             amount = isObject(completed) ? completed.amount : completed,
+    //             status = ((amount || 0) * 100) + '%',
+    //             lines;
+
+    //         lines = [{
+    //             value: point.name,
+    //             style: 'font-weight: bold;'
+    //         }, {
+    //             title: 'Start',
+    //             value: dateFormat(format, point.start)
+    //         }, {
+    //             visible: !options.milestone,
+    //             title: 'End',
+    //             value: dateFormat(format, point.end)
+    //         }, {
+    //             title: 'Completed',
+    //             value: status
+    //         }, {
+    //             title: 'Owner',
+    //             value: options.owner || 'unassigned'
+    //         }];
+
+    //         return reduce(lines, function (str, line) {
+    //             var s = '',
+    //                 style = (
+    //                     defined(line.style) ? line.style : 'font-size: 0.8em;'
+    //                 );
+    //             if (line.visible !== false) {
+    //                 s = (
+    //                     '<span style="' + style + '">' +
+    //                     (defined(line.title) ? line.title + ': ' : '') +
+    //                     (defined(line.value) ? line.value : '') +
+    //                     '</span><br/>'
+    //                 );
+    //             }
+    //             return str + s;
+    //         }, '');
+    //     }
+    // },
+    title: {
+        text: 'Gantt Project Management'
+    },
+    xAxis: {
+        currentDateIndicator: true,
+        // min: today - 3 * day,
+        // max: today + 18 * day
+    }
+});
 
   }
 }
